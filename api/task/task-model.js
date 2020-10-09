4edff789bec1ea4524796aed527af3f155d8c430
Elijah Kim <elijahkim@Elijahs-MacBook-Pro.local>
2020-10-09 13:46:48 -0400

    tasks now return project name, description

--- api/task/task-model.js
+++ api/task/task-model.js
@@ -7,7 +7,14 @@ module.exports = {
 }
 
 function getAll() {
-  return db('task');
+  return db('task')
+    .select('task.id',
+      'task.task_description',
+      'task,task_completed',
+      'project.project_name',
+      'project.project_description'
+    )
+    .join('project', 'project.id', 'task.project_id');
 }
 
 function getById(id) {
